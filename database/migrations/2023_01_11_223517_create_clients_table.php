3b3f78575483694a0b5df1f39a837b1d6ca2a07a
Bruno P.M <brunoporras2001@gmail.com>
2023-01-11 17:39:49 -0500

    Modelo de cliente creado

---
+++ database/migrations/2023_01_11_223517_create_clients_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->string('cliente', 150)->default(null);
+            $table->string('correo', 100)->default(null);
+            $table->integer('notificacion')->default(null);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+};
